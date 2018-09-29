3f85b845ccc60ebd30ecdcb0cc666120bc72745a
Unknown <cihankurt1998@gmail.com>
2018-09-29 20:01:24 +0200

    UART Begin
    GurfluntUart

---
+++ DES/Week 3  - UART/Code/UART/Receiver.ino
@@ -0,0 +1,49 @@
+char receiver_ReadBit()
+{
+  return digitalRead(rxPin);
+}
+
+void receiver_ReadData()
+{
+  for (int bufferPosition = 0; bufferPosition < maxReceivingDataSize; bufferPosition++)
+  {
+    receivedData[bufferPosition] = receiver_ReadBit(); //Add new databit on every position of the buffer (only databits)
+  }
+}
+
+bool receiver_CheckParity(int parityBit)
+{
+  //Parity has to be uneven.
+  int oneCount = 0;
+
+  //count the amount of '1's
+  for (int i = 0; i < maxReceivingDataSize; i++)
+  {
+    if (receivedData[i] == 1)
+    {
+      oneCount++;
+    }
+  }
+
+  //Check if paritybit is 1
+  if (parityBit == 1)
+  {
+    oneCount++;
+  }
+
+  //Check if it's even or odd by dividing by 2.
+  if (oneCount % 2 == 1) //odd
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void receiver_FixData()
+{
+  
+}
+
---
+++ DES/Week 3  - UART/Code/UART/UART.ino
@@ -0,0 +1,71 @@
+//Some variables to prevent magic numbers
+const int baudRate = 9600;
+const int startBit = 0;
+const int stopBit = 1;
+const int maxReceivingDataSize = 8;
+
+//switch case states
+int cases = 0;
+
+//Buffers
+int receivedData[maxReceivingDataSize]; // size of data including stopbit or not? Data is between 5-9 databits according to the slides.
+
+//bools for states, can be deleted afterwards I guess.
+bool receiving = false;
+bool sending = false;
+
+//RX & TX Pins
+const int rxPin = 10;
+const int txPin = 11;
+
+//States for Receiving & Sending
+enum states
+{
+  waitingForStartBit, // High to Low | 1 > 0
+  readData, //Read the data and put it in a buffer
+  checkingParity, // Odd or Even
+  clearEverything
+};
+
+
+
+void setup()
+{
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+
+  cases = waitingForStartBit; //waitingForStartBit is 0 translated, a.k.a. begin state.
+
+  Serial.begin(baudRate);
+}
+
+void loop()
+{
+  switch (cases)
+  {
+    case waitingForStartBit:
+      if ((int) receiver_ReadBit() == startBit)
+      {
+        cases = readData;
+      }
+      break;
+      
+    case readData:
+      receiver_ReadData();
+      cases = checkingParity;
+      break;
+      
+    case checkingParity:
+      int parityBit = (int) receiver_ReadBit();
+      if (receiver_CheckParity(parityBit) == true)
+      {
+        cases = waitingForEndBit; //state can be removed
+      }
+      else
+      {
+        cases = clearEverything; //wait for next byte
+      }
+      
+      break;
+  }
+}
