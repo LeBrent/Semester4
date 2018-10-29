bfb7e66420206c69316b889eb29f27c6fa275916
Brent Schoenmakers <kofenoen@outlook.com>
2018-10-29 15:43:41 +0100

    added matlab file for batchprocessing

---
+++ PRS/Batchprocessing.m
@@ -0,0 +1,34 @@
+% PID controller example
+% 2nd Order Process / IRT005 / 
+% O.Figaroa 
+%
+close all; 
+clearvars;                  % Clear all variables
+num=[1];                   % numerator
+den =[625 50 1];             % denumerator
+Gopen=tf(num,den);          % Transfer function open loop
+figure(1);                  % Create figure 1 for the step response
+step(Gopen);                % step response open loop
+grid on                     %
+hold on                     % figure 1 on hold for next responses in the same figure
+Contr= pidtune(Gopen,'PID');% Tuning Kp, Ki and Kd
+%Show parameters obtained by matlab tool
+X =('Obtained PID parameters:');
+disp(X);
+Contr
+% Closed loop with Obtained PID
+Gclose= feedback(Contr*Gopen,1);
+step(Gclose);               %  Step response
+% = = = = = = = = = = = = = = = =
+% PID parameters modified by User = =
+% obtained by command >> pidTuner(Gopen);
+%
+Contr.Kp=18.92;               % Kp - parameter modified by User
+Contr.Ki=0.06236;               % Ki - parameter modified by User
+Contr.Kd=127.3;              % Kd - parameter modified by User
+% Closed loop 
+Gclos2= feedback(Contr*Gopen,1);
+step(Gclos2);
+title(['PID Controller Kp = ',num2str(Contr.Kp),' Ki = ',num2str(Contr.Ki),' Kd = ',num2str(Contr.Kd)]);
+legend('Open-loop response','Standard PID response', 'Modified PID response');
+pidTuner(Gopen);
